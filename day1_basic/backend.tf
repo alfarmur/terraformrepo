df1178f34af0ac493439ba57f6b6c2e8db3f98a6
alfarmur <yamar2k@yahoo.com>
2025-06-29 22:52:50 +0530

    dynamo

--- day1_basic/backend.tf
+++ day1_basic/backend.tf
@@ -1,7 +1,9 @@
-# terraform {
-#   backend "s3" {
-    # bucket = "irumporaiamma"
-    # key    = terraform.tfstate
-    # region = "ap-south-1"
-#   }
-# }
+ terraform {
+   backend "s3" {
+ bucket = "irumporaiamma"
+ key    = "terraform.tfstate"
+ region = "ap-south-1"
+ dynamodb_table = "terraform_state_lock_dynamo"
+ encrypt = true
+   }
+ }
--- day1_basic/main.tf
+++ day1_basic/main.tf
@@ -3,4 +3,16 @@ resource "aws_instance" "dev" {
   ami           = var.ami_id
   instance_type = var.instance_type
   key_name      = var.key_name
+
+  tags = { name = "test"}
+}
+
+resource "aws_dynamodb_table" "dynamodb_terraform" {
+  name = "terraform_state_lock"
+  hash_key = "lock_id"
+  read_capacity = 20
+  write_capacity = 20
+  attribute {
+    name = "lock_id"
+  }
 }
